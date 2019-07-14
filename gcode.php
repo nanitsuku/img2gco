3b9bf821820232dacdde0ba2c83f731a2bbde5b2
nanitsuku <nanitsuku@outlook.jp>
2019-07-14 16:59:15 +0900

    obsolete function deleted due to use php7

--- gcode.php
+++ gcode.php
@@ -10,33 +10,6 @@
  * @return bool
  * @require PHP 3.0.7 (function_exists), GD1
  */
-function flipMyImage(&$image, $x = 0, $y = 0, $width = null, $height = null)
-{
-    if ($width  < 1) $width  = imagesx($image);
-    if ($height < 1) $height = imagesy($image);
-    // Truecolor provides better results, if possible.
-    if (function_exists('imageistruecolor') && imageistruecolor($image))
-    {
-        $tmp = imagecreatetruecolor(1, $height);
-    }
-    else
-    {
-        $tmp = imagecreate(1, $height);
-    }
-    $x2 = $x + $width - 1;
-    for ($i = (int) floor(($width - 1) / 2); $i >= 0; $i--)
-    {
-        // Backup right stripe.
-        imagecopy($tmp,   $image, 0,        0,  $x2 - $i, $y, 1, $height);
-        // Copy left stripe to the right.
-        imagecopy($image, $image, $x2 - $i, $y, $x + $i,  $y, 1, $height);
-        // Copy backuped right stripe to the left.
-        imagecopy($image, $tmp,   $x + $i,  $y, 0,        0,  1, $height);
-    }
-    imagedestroy($tmp);
-    return true;
-}
-
 function map($value, $fromLow, $fromHigh, $toLow, $toHigh) {
     $fromRange = $fromHigh - $fromLow;
     $toRange = $toHigh - $toLow;
@@ -135,7 +108,6 @@ imagecopyresampled($tmp, $src, 0, 0, 0, 0, $pixelsX, $pixelsY, $w, $h);
 
 if(isset($_POST['flipImage']))
   {
-//  flipMyImage($tmp);
   imageflip($tmp, IMG_FLIP_VERTICAL);
   }
 imagefilter($tmp,IMG_FILTER_GRAYSCALE);
